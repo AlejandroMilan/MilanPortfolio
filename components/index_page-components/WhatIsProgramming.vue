dded7cebfc47d39437265710bf4de7cb326aefd8
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-31 18:19:30 -0500

    Animations created for What Is Programming component

--- components/index_page-components/WhatIsProgramming.vue
+++ components/index_page-components/WhatIsProgramming.vue
@@ -2,8 +2,10 @@
   <div class="what-is-programming container">
     <h1 class="font-weight-bold">
       Web development is
-      <transition name="fade"
-        ><span>{{ adjectives[adjectivePosition] }}</span></transition
+      <transition name="slide-fade" mode="out-in"
+        ><span :key="adjectives[adjectivePosition]">{{
+          adjectives[adjectivePosition]
+        }}</span></transition
       >
     </h1>
   </div>
@@ -14,6 +16,16 @@
   width: 100%
   text-align: center
   text-transform: uppercase
+
+.slide-fade-enter-active
+  transition: all .3s ease
+
+.slide-fade-leave-active
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+
+.slide-fade-enter, .slide-fade-leave-to
+  transform: translateX(10px)
+  opacity: 0
 </style>
 
 <script lang="ts">
@@ -24,6 +36,12 @@ export default Vue.extend({
       adjectivePosition: 0,
       adjectives: ['interesting', 'awesome', 'an art'],
     }
+  },
+
+  mounted() {
+    setTimeout(() => {
+      this.adjectivePosition++
+    }, 2000)
   },
 })
 </script>
