0fc48ebdce235c7a1f90054144d3aabf95c7c07d
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-31 17:01:06 -0500

    What is programming component ready for changes

---
+++ components/index_page-components/WhatIsProgramming.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="what-is-programming container">
+    <h1 class="font-weight-bold">Web development is</h1>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.what-is-programming
+  width: 100%
+  text-align: center
+  text-transform: uppercase
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
