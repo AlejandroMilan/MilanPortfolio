5d0e62d47166a5e4d94f96dc3a6456ad1441be61
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-31 16:41:43 -0500

    Main background of index changed to black

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,13 @@
   <main></main>
 </template>
 
+<style lang="sass" scoped>
+main
+  min-width: 100%
+  min-height: 100vh;
+  background-color: var(--primary)
+</style>
+
 <script lang="ts">
 import Vue from 'vue'
 
