54497944582dd833fed3ce85ee058459d2b67ded
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-30 23:55:44 -0500

    Nav links added to data of the component

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -6,5 +6,28 @@
 import Vue from 'vue'
 export default Vue.extend({
   name: 'Navbar',
+
+  data: () => {
+    return {
+      navLinks: [
+        {
+          name: 'Home',
+          destination: '/',
+        },
+        {
+          name: 'About me',
+          destination: '/about',
+        },
+        {
+          name: 'My services',
+          destination: '/services',
+        },
+        {
+          name: 'Skills',
+          destination: '/skills',
+        },
+      ],
+    }
+  },
 })
 </script>
