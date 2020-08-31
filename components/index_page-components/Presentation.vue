e02c5070827ced8e9ed2de2951a5aac699a34c55
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-31 18:53:47 -0500

    Presentation component created

---
+++ components/index_page-components/Presentation.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="presentation container">
+    <h2>Hello, my name is Alejandro and i am interested to work with you.</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
