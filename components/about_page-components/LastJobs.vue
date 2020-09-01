2fe71fec75a0850b818544a8099a4771d60f6c85
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 11:18:53 -0500

    Last jobs items working

--- components/about_page-components/LastJobs.vue
+++ components/about_page-components/LastJobs.vue
@@ -2,6 +2,27 @@
   <section id="last-jobs">
     <div class="container">
       <h2 class="font-weight-bold">{{ title }}</h2>
+      <div class="last-jobs-container">
+        <article
+          v-for="(job, index) in jobs"
+          :key="index"
+          class="last-jobs-item"
+        >
+          <h3 class="font-weight-bold">{{ job.title }}</h3>
+          <h4>{{ job.employer }}</h4>
+          <small>{{ job.initDate }} to {{ job.finishDate }}</small>
+          <p>{{ job.description }}</p>
+          <div class="tech-used">
+            <strong>I used:</strong>
+            <b-badge
+              v-for="tech in job.technologiesUsed"
+              :key="tech"
+              variant="primary"
+              >{{ tech }}</b-badge
+            >
+          </div>
+        </article>
+      </div>
     </div>
   </section>
 </template>
@@ -12,6 +33,16 @@ export default Vue.extend({
   data: () => {
     return {
       title: 'My last jobs',
+      jobs: [
+        {
+          title: 'Web Developer',
+          employer: 'Grupo SMIN',
+          initDate: 'November, 2016',
+          finishDate: 'March, 2018',
+          description: 'Build a responsive website with catalog integrated.',
+          technologiesUsed: ['HTML', 'CSS', 'JS', 'PHP', 'MySQL'],
+        },
+      ],
     }
   },
 })
