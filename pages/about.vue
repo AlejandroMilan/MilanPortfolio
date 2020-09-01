020717547d85b5c2f2791aadb8c772e97bd16a4f
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 10:54:42 -0500

    Last jobs component added to about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <AboutMe></AboutMe>
+    <LastJobs></LastJobs>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import AboutMe from '~/components/about_page-components/AboutMe.vue'
+import LastJobs from '~/components/about_page-components/LastJobs.vue'
 
 export default Vue.extend({
   components: {
     AboutMe,
+    LastJobs,
   },
 })
 </script>
