a46fe9b71ab88c8084dd070ca69d0b98a6a5b1ba
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-31 19:16:23 -0500

    CTA button imported in Presentation component

--- components/index_page-components/Presentation.vue
+++ components/index_page-components/Presentation.vue
@@ -3,6 +3,9 @@
     <h2 class="font-weight-light h5">
       Hello, my name is Alejandro and I am interested to work with you.
     </h2>
+    <div class="container">
+      <CTAButton></CTAButton>
+    </div>
   </div>
 </template>
 
@@ -14,5 +17,11 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import CTAButton from '~/components/layout/CTAButton.vue'
+
+export default Vue.extend({
+  components: {
+    CTAButton,
+  },
+})
 </script>
