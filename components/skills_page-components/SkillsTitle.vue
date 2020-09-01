10537464a51430c06e8a1808431960ce38ecc627
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 12:33:14 -0500

    Introduction added to skill title component

--- components/skills_page-components/SkillsTitle.vue
+++ components/skills_page-components/SkillsTitle.vue
@@ -2,6 +2,7 @@
   <section class="title">
     <div class="container">
       <h1 class="font-weight-bold">{{ title }}</h1>
+      <p class="font-weight-light">{{ introduction }}</p>
     </div>
   </section>
 </template>
@@ -17,7 +18,9 @@ import Vue from 'vue'
 export default Vue.extend({
   data: () => {
     return {
-      title: 'My principal skills'
+      title: 'My principal skills',
+      introduction:
+        'Here is some knowledge that i have and the corresponding experience time of each.',
     }
   },
 })
