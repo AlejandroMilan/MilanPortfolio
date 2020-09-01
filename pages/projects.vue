8afa8313ab771d923864c8412173a96819b35047
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 16:32:00 -0500

    Project list component added to projects page

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <ProjectsTitle></ProjectsTitle>
+    <ProjectList></ProjectList>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import ProjectsTitle from '~/components/projects_page-components/ProjectsTitle.vue'
+import ProjectList from '~/components/projects_page-components/ProjectList.vue'
 
 export default Vue.extend({
   components: {
     ProjectsTitle,
+    ProjectList,
   },
 })
 </script>
