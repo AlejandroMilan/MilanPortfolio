2d242f480d5bdbf5540e8ef064958445e28846b5
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 12:27:24 -0500

    Skills title component created

---
+++ components/skills_page-components/SkillsTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="title">
+    <div class="container">
+      <h1 class="font-weight-bold">{{ title }}</h1>
+    </div>
+  </section>
+</template>
+
+<style lang="sass" scoped>
+.title
+  width: 100%
+  text-align: center
+</style>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => {
+    return {
+      title: 'My principal skills'
+    }
+  },
+})
+</script>
