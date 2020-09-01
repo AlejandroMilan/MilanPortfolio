2b02d3be95e336ae033dac7b239ccb65f99f7259
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 14:23:37 -0500

    Title data prop added to other skills component

--- components/skills_page-components/OtherSkills.vue
+++ components/skills_page-components/OtherSkills.vue
@@ -1,8 +1,18 @@
 <template>
-  <h1>Other skills</h1>
+  <section class="other-skills">
+    <div class="title container">
+      <h2 class="font-weight-bold">{{ title }}</h2>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => {
+    return {
+      title: 'Other skills',
+    }
+  },
+})
 </script>
