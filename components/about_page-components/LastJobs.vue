f603cd09d17699dd96705a3dccd4344e60262391
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 11:00:03 -0500

    Component title added to my last jobs

--- components/about_page-components/LastJobs.vue
+++ components/about_page-components/LastJobs.vue
@@ -1,8 +1,18 @@
 <template>
-  <h1>Last jobs</h1>
+  <section id="last-jobs">
+    <div class="container">
+      <h2 class="font-weight-bold">{{ title }}</h2>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => {
+    return {
+      title: 'My last jobs',
+    }
+  },
+})
 </script>
