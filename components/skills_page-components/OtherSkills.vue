d4dc2e9859e393b2b3432ab0284c6b6e1fed1b66
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 14:32:52 -0500

    Other skills data added to the component

--- components/skills_page-components/OtherSkills.vue
+++ components/skills_page-components/OtherSkills.vue
@@ -3,8 +3,22 @@
     <div class="title container">
       <h2 class="font-weight-bold">{{ title }}</h2>
     </div>
+    <div class="badges">
+      <b-badge v-for="skill in skills" :key="skill" variant="primary">{{
+        skill
+      }}</b-badge>
+    </div>
   </section>
 </template>
+
+<style lang="sass">
+.other-skills
+  padding: 2rem 0
+  text-align: center
+
+  .badge
+    margin: 0 2px
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
@@ -12,6 +26,13 @@ export default Vue.extend({
   data: () => {
     return {
       title: 'Other skills',
+      skills: [
+        'SQL',
+        'MySQL',
+        'MongoDB',
+        'Firebase services',
+        'REST APIs Development',
+      ],
     }
   },
 })
