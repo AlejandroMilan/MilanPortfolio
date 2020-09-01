60d129aa479393eedb95d06170ed31e34a642c0e
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 13:54:56 -0500

    Icons used in skill list component

--- components/skills_page-components/SkillList.vue
+++ components/skills_page-components/SkillList.vue
@@ -1,10 +1,12 @@
 <template>
   <section class="skill-list">
     <div class="container">
-      <article class="skill-list-item">
-        <div class="skill-list-item-icon"></div>
+      <article class="skill-list-item" v-for="skill in skills" :key="skill.name">
+        <div class="skill-list-item-icon">
+	  <font-awesome-icon :icon="[skill.iconType, skill.iconName]" />
+	</div>
         <div class="skill-list-item-title">
-          <h2></h2>
+          <h2>{{skill.name}}</h2>
         </div>
       </article>
     </div>
@@ -13,6 +15,12 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHtml5, faCss3, faJsSquare, faPhp, faNode, faVuejs, faAngular } from '@fortawesome/free-brands-svg-icons'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+library.add(faHtml5, faCss3, faJsSquare, faPhp, faNode, faVuejs, faAngular)
+
 export default Vue.extend({
   data: () => {
     return {
@@ -25,7 +33,7 @@ export default Vue.extend({
         },
         {
           name: 'CSS',
-          iconName: 'css3-alt',
+          iconName: 'css3',
           iconType: 'fab',
           experienceTime: '4 years',
         },
