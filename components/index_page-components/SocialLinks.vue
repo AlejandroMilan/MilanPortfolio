bce1541dd002815d904ae0b98f46e3fbdbbac236
Alejandro Milán <alex.13062001des@gmail.com>
2020-08-31 22:24:30 -0500

    Vue-fontawesome in use successfuly

--- components/index_page-components/SocialLinks.vue
+++ components/index_page-components/SocialLinks.vue
@@ -2,6 +2,7 @@
   <div class="social-links container">
     <article class="social-links-item">
       <a class="social-link-item-icons">
+        <font-awesome-icon :icon="['fas', 'envelope-open-text']" />
       </a>
       <div class="social-link-item-title">
         <h3 class="h5"></h3>
@@ -12,5 +13,15 @@
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+// import { name of your icon in camelCase } from "@fortawesome/free-solid-svg-icons";
+// For example, I want to use fa-enveloper-open-text, then it's faEnvelopeOpenText
+import { faEnvelopeOpenText } from '@fortawesome/free-solid-svg-icons'
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+// Then add it to library
+library.add(faEnvelopeOpenText)
+export default Vue.extend({
+  components: {},
+})
 </script>
