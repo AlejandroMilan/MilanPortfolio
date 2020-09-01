337ea22ecaf4e7502e90145e0eccc7b34162de58
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 08:42:03 -0500

    Styles for social links done

--- components/index_page-components/SocialLinks.vue
+++ components/index_page-components/SocialLinks.vue
@@ -5,26 +5,36 @@
       :key="socialLink.name"
       class="social-links-item"
     >
-      <a class="social-link-item-icons" href="socialLink.url">
+      <a class="social-link-item-icon" href="socialLink.url">
         <font-awesome-icon :icon="['fab', socialLink.iconName]" />
       </a>
       <div class="social-link-item-title">
-        <h3 class="h5">{{ socialLink.name }}</h3>
+        <h3 class="h6">{{ socialLink.name }}</h3>
       </div>
     </article>
   </div>
 </template>
 
+<style lang="sass" scoped>
+.social-links
+  width: 100%
+  padding: 2rem 0
+  display: flex
+  flex-wrap: wrap
+  justify-content: center
+  text-align: center
+  column-gap: 1rem
+
+  .social-link-item-icon
+    font-size: 2rem
+</style>
+
 <script lang="ts">
 import Vue from 'vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-// import { name of your icon in camelCase } from "@fortawesome/free-solid-svg-icons";
-// For example, I want to use fa-enveloper-open-text, then it's faEnvelopeOpenText
-// import { github } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-// Then add it to library
 library.add(faGithub, faLinkedin)
 export default Vue.extend({
   data: () => {
