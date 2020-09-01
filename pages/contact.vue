13096b303be7fc540c52b9ff724b995d04f85ab2
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 18:41:10 -0500

    Contact title added to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,16 +1,23 @@
 <template>
   <main>
-    <ContactForm></ContactForm>
+    <div class="left">
+      <ContactTitle></ContactTitle>
+    </div>
+    <div class="right">
+      <ContactForm></ContactForm>
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import ContactForm from '~/components/contact_page-components/ContactForm.vue'
+import ContactTitle from '~/components/contact_page-components/ContactTitle.vue'
 
 export default Vue.extend({
   components: {
     ContactForm,
+    ContactTitle,
   },
 })
 </script>
