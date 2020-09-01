57679c28d593ac69a4ff03f3a708049543d45f58
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 18:17:53 -0500

    Contact form component added to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,16 @@
 <template>
-  <h1>Contact</h1>
+  <main>
+    <ContactForm></ContactForm>
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+import ContactForm from '~/components/contact_page-components/ContactForm.vue'
+
+export default Vue.extend({
+  components: {
+    ContactForm,
+  },
+})
 </script>
