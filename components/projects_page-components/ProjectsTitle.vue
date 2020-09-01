d37d0db8d5046c366c83b3d1a780f5dbc104f0e4
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 16:25:25 -0500

    Projects title component ready

--- components/projects_page-components/ProjectsTitle.vue
+++ components/projects_page-components/ProjectsTitle.vue
@@ -1,8 +1,25 @@
 <template>
-  <h1>Titulo</h1>
+  <section class="projects-title">
+    <div class="container">
+      <h1 class="font-weight-bold">{{ title }}</h1>
+    </div>
+  </section>
 </template>
+
+<style lang="sass" scoped>
+.projects-title .container
+  padding-top: 2rem
+  padding-bottom: 2rem
+  text-align: center
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => {
+    return {
+      title: 'Some projects made by me'
+    }
+  },
+})
 </script>
