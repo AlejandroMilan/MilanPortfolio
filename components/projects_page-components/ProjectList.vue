c69d8cc30e4a1715304b741cb461ced503abcbf2
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 17:04:32 -0500

    Project list component template ready for data

--- components/projects_page-components/ProjectList.vue
+++ components/projects_page-components/ProjectList.vue
@@ -1,5 +1,24 @@
 <template>
-  <h1>Projects</h1>
+  <section class="project-list">
+    <div class="container">
+      <article class="project-list-item">
+        <div class="project-list-item-image">
+          <img src="" alt="Project image" />
+        </div>
+        <div class="project-list-item-description">
+          <h2></h2>
+          <p></p>
+          <div class="technologies-used">
+            <small>Technologies used:</small>
+            <b-badge variant="primary"></b-badge>
+          </div>
+          <div class="repo-button">
+            <a href="" class="btn btn-primary">View in github</a>
+          </div>
+        </div>
+      </article>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
