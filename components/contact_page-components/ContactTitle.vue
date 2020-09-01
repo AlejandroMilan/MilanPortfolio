1ef9cd100bae7284b98cc205fe2b5364053c1db6
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 18:49:59 -0500

    Data added to Contact title component

--- components/contact_page-components/ContactTitle.vue
+++ components/contact_page-components/ContactTitle.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>title</h1>
+  <section class="contact">
+    <div class="container">
+      <h1 class="font-weight-bold">{{ title }}</h1>
+      <p class="font-weight-light">{{ introduction }}</p>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => {
+    return {
+      title: "Let's work together",
+      introduction:
+        'Just fill the form and i will contact with you. Thank you for your interest.',
+    }
+  },
+})
 </script>
