56d11f2a0552cc367165d4db2c795f59065773a8
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 18:34:18 -0500

    ContactForm built

--- components/contact_page-components/ContactForm.vue
+++ components/contact_page-components/ContactForm.vue
@@ -1,8 +1,59 @@
 <template>
-  <h1>Form</h1>
+  <section class="contact-form">
+    <div class="container">
+      <form action="#" @submit="sendForm">
+        <b-form-group>
+          <b-form-input
+            id="input-first_name"
+            v-model="form.firstName"
+            type="text"
+            required
+            placeholder="Your first name"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group>
+          <b-form-input
+            id="input-last_name"
+            v-model="form.lastName"
+            type="text"
+            required
+            placeholder="Your first name"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group>
+          <b-form-input
+            id="input-email"
+            v-model="form.email"
+            type="email"
+            required
+            placeholder="Your email"
+          ></b-form-input>
+        </b-form-group>
+        <b-form-group>
+          <b-form-textarea
+            id="textarea-message"
+            v-model="form.message"
+            placeholder="Your message"
+            rows="3"
+          ></b-form-textarea>
+        </b-form-group>
+      </form>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => {
+    return {
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        message: '',
+      },
+    }
+  },
+})
 </script>
