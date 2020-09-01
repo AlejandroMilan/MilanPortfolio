dc82283c843971e4f91427b14a41b00b8ecbbd47
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 09:36:43 -0500

    Padding top added to nuxt default template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <Navbar></Navbar>
-    <Nuxt />
+    <Nuxt class="nuxt" />
   </div>
 </template>
+
+<style lang="sass" scoped>
+.nuxt
+  padding-top: 100px
+  @media(min-width: 850px)
+    padding-top: 150px
+  @media(min-width: 992px)
+    padding-top: 100px
+</style>
 
 <script lang="ts">
 import Vue from 'vue'
