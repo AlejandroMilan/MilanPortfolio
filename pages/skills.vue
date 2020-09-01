0111de2edfa77d618bcdf0111e8bd35be2dc8043
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-01 12:38:46 -0500

    Skill list component added to skill page

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,16 +1,19 @@
 <template>
   <main>
     <SkillsTitle></SkillsTitle>
+    <SkillList></SkillList>
   </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import SkillsTitle from '~/components/skills_page-components/SkillsTitle.vue'
+import SkillList from '~/components/skills_page-components/SkillList.vue'
 
 export default Vue.extend({
   components: {
     SkillsTitle,
+    SkillList,
   },
 })
 </script>
