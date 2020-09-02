718d4feec8338f2adf25160256f6a88eef87fe00
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-02 14:30:11 -0500

    Other contact medias component added to Contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,6 +2,7 @@
   <main>
     <ContactTitle></ContactTitle>
     <ContactForm></ContactForm>
+    <OtherContactMedias></OtherContactMedias>
   </main>
 </template>
 
@@ -9,11 +10,13 @@
 import Vue from 'vue'
 import ContactForm from '~/components/contact_page-components/ContactForm.vue'
 import ContactTitle from '~/components/contact_page-components/ContactTitle.vue'
+import OtherContactMedias from '~/components/contact_page-components/OtherContactMedias.vue'
 
 export default Vue.extend({
   components: {
     ContactForm,
     ContactTitle,
+    OtherContactMedias,
   },
 })
 </script>
