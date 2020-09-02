f0151018271eb58712109df6a39ddff1a70e02c3
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-02 15:27:35 -0500

    Contact page media queries added

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,11 +1,32 @@
 <template>
   <main>
-    <ContactTitle></ContactTitle>
-    <ContactForm></ContactForm>
-    <OtherContactMedias></OtherContactMedias>
+    <ContactTitle class="title"></ContactTitle>
+    <ContactForm class="contact-form"></ContactForm>
+    <OtherContactMedias class="other-contact-ways"></OtherContactMedias>
   </main>
 </template>
 
+<style lang="sass" scoped>
+main
+  @media(min-width: 992px)
+    max-width: 960px
+    margin: 0 auto
+    display: grid
+    grid-template-columns: 50% 50%
+    grid-template-areas: "t f" "o f"
+
+  .title
+    @media(min-width: 992px)
+      grid-area: t
+
+  .contact-form
+    @media(min-width: 992px)
+      grid-area: f
+
+  .other-contact-ways
+    @media(min-width: 992px)
+      grid-area: o
+</style>
 <script lang="ts">
 import Vue from 'vue'
 import ContactForm from '~/components/contact_page-components/ContactForm.vue'
