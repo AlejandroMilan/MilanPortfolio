fbdb56067bad997092434ec80944e2aea5d0cf82
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-02 15:42:14 -0500

    Social links added to contact page

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,6 +3,7 @@
     <ContactTitle class="title"></ContactTitle>
     <ContactForm class="contact-form"></ContactForm>
     <OtherContactMedias class="other-contact-ways"></OtherContactMedias>
+    <SocialLinks class="social-links"></SocialLinks>
   </main>
 </template>
 
@@ -13,7 +14,7 @@ main
     margin: 0 auto
     display: grid
     grid-template-columns: 50% 50%
-    grid-template-areas: "t f" "o f"
+    grid-template-areas: "t f" "o f" "sl sl"
 
   .title
     @media(min-width: 992px)
@@ -26,18 +27,25 @@ main
   .other-contact-ways
     @media(min-width: 992px)
       grid-area: o
+
+  .social-links
+    @media(min-width: 992px)
+      grid-area: sl
+      margin-top: 5rem
 </style>
 <script lang="ts">
 import Vue from 'vue'
 import ContactForm from '~/components/contact_page-components/ContactForm.vue'
 import ContactTitle from '~/components/contact_page-components/ContactTitle.vue'
 import OtherContactMedias from '~/components/contact_page-components/OtherContactMedias.vue'
+import SocialLinks from '~/components/index_page-components/SocialLinks.vue'
 
 export default Vue.extend({
   components: {
     ContactForm,
     ContactTitle,
     OtherContactMedias,
+    SocialLinks,
   },
 })
 </script>
