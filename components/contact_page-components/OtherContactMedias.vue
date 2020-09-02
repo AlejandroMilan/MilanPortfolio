6462c1c5d560ff47df4457f1d46c4d9dd8bcaf82
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-02 14:46:28 -0500

    Other contact media component data filled

--- components/contact_page-components/OtherContactMedias.vue
+++ components/contact_page-components/OtherContactMedias.vue
@@ -1,8 +1,39 @@
 <template>
-  <h1>Other medias</h1>
+  <section class="other-contact-medias">
+    <div class="container">
+      <h2>Other ways to contact with me</h2>
+      <div class="contact-medias">
+        <article
+          v-for="(contactMedia, index) in contactMedias"
+          :key="index"
+          class="contact-medias-item"
+        >
+          <div class="contact-media-item-icon"></div>
+          <div class="contact-media-item-content">
+            {{ contactMedia.content }}
+          </div>
+        </article>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-export default Vue.extend({})
+export default Vue.extend({
+  data: () => {
+    return {
+      contactMedias: [
+        {
+          content: 'alejandro.milan.serrano@gmail.com',
+          iconName: '',
+        },
+        {
+          content: '+52 427-227-8479',
+          iconName: '',
+        },
+      ],
+    }
+  },
+})
 </script>
