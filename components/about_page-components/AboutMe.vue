720fa2f2f593f10e4285f0ffa751adff3a9e24e6
Alejandro Milán <alex.13062001des@gmail.com>
2020-09-03 16:19:09 -0500

    About me text fixed

--- components/about_page-components/AboutMe.vue
+++ components/about_page-components/AboutMe.vue
@@ -48,7 +48,7 @@ export default Vue.extend({
       title: 'Front-End Developer with 4 years of experience',
       paragraphs: [
         'My full name is Alejandro Milán Serrano, I am from Mexico and I am 19 years old. I like to play guitar and do sports in my free time.',
-        'I am a technology lover and my passion for programming started at 13 years when I tried to do my own videogames, that is when i learned about videogames development with Unity, C# and Javascript.',
+        'I am a technology lover and my passion for programming started at 13 years when I tried to do my own videogames, that is when I learned about videogames development with Unity, C# and Javascript.',
         'At my 15 years old I was required to build a website to a Real State business, wich had to include a catalog of properties. Since then I am dedicating to web development.',
       ],
     }
